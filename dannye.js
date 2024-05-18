5b914ef14e021fb3f7305969404fa28e8efdf267
gayworld <alkojmm@zohomail.eu>
2024-05-19 01:17:07 +0300

    Update dannye.js

--- dannye.js
+++ dannye.js
@@ -1,4 +1,1 @@
-var name_id4099382 = "Иванов Василий"; document.getElementById("name_id4099382").innerHTML = name_id4099382;
-var age_id4099382 = "Возраст: 54"; document.getElementById("age_id4099382").innerHTML = age_id4099382;
-var color_id4099382 = "Цвет волос: лысый"; document.getElementById("color_id4099382").innerHTML = color_id4099382;
-var body_id4099382 = "Телосложение: спортивное."; document.getElementById("body_id4099382").innerHTML = body_id4099382;
+var name_id4099382 = "Иванов Василий"; document.getElementById("name_id4099382").innerHTML = name_id4099382; var age_id4099382 = "Возраст: 54"; document.getElementById("age_id4099382").innerHTML = age_id4099382; var color_id4099382 = "Цвет волос: лысый"; document.getElementById("color_id4099382").innerHTML = color_id4099382; var body_id4099382 = "Телосложение: спортивное."; document.getElementById("body_id4099382").innerHTML = body_id4099382;
